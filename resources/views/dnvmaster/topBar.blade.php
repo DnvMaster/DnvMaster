41483d899fc8aeabd9d7e05da315107075f304a8
Dnvmaster <dnvcomp@yandexru>
2023-02-25 10:29:49 +0300

    102825022023 Top panel

--- resources/views/dnvmaster/topBar.blade.php
+++ resources/views/dnvmaster/topBar.blade.php
@@ -1,14 +1,16 @@
 <div id="topBar" class="d-none d-sm-block">
-    <div class="container">
+    <div class="container text-center">
         <div class="row">
-            <div class="col-9">
-                <div class="topBar-info">
-                    <div><i class="fa fa-clock-four"></i>&nbsp;<?=date('d-m-Y'); ?></div>
-                </div>
+            <div class="col-9 col-md-9">
+                <ul class="topBar-left">
+                    <li><i class="fa fa-clock-four"></i><?=date('d F y - H:i')?></li>
+                </ul>
             </div>
-            <div class="col-3">
-                <a href="#"><i class="fa fa-user-shield"></i>&nbsp;Логин</a>
-                <a href="#"><i class="fa fa-user-pen"></i>&nbsp;Регистрация</a>
+            <div class="col-3 col-md-3">
+                <ul class="topBar-right">
+                    <li><i class="fa fa-user-check"></i><a href="#">Логин</a></li>
+                    <li><i class="fa fa-user-pen"></i><a href="#">Регистрация</a></li>
+                </ul>
             </div>
         </div>
     </div>
